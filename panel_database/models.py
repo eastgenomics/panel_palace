99f8debb8c3e1980581ef719146aa38968f47a3e
Yu-jinKim <yujin.kim@hotmail.fr>
2021-06-21 14:02:38 +0100

    version for clinical indication in the clinicalIndicationPanels table

--- panel_database/models.py
+++ panel_database/models.py
@@ -26,6 +26,7 @@ class ClinicalIndicationPanels(models.Model):
         ClinicalIndication, on_delete=models.DO_NOTHING
     )
     panel = models.ForeignKey("Panel", on_delete=models.DO_NOTHING)
+    version = models.CharField(max_length=100)
 
     class Meta:
         db_table = "clinical_indication_panels"
